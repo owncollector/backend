0d13204eb6cc5476bcc6736422c90184ce85bd73
samcasas <samycasas1@gmail.com>
2024-11-30 21:49:10 -0600

    ajustes nuevos

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,8 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -24,5 +22,3 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
-
-    L5Swagger\L5SwaggerServiceProvider::class;
